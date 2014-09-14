5f6b0303af015899d4d3b1261c576142e1d27c95
xiaobengdou <jacklinshi1004@gmail.com>
2014-09-14 16:08:54 -0700

    zoj 2177

---
+++ algo/kmp.cc
@@ -0,0 +1,38 @@
+
+int next[MAX_N];
+// The following illustration comes from http://www.cnblogs.com/yjiyjige/p/3263858.html
+//   0   k j-k j       0   k j-k j
+//   |   | |   |       |   | |   |
+//   A B C A B C D     A B C A B C D
+//     |     |           |   | |   |
+//    k-1   j-1        k-1 k+1 j-1 j+1
+//
+void get_next(char str[], int len) {
+    next[0] = -1;
+    int i = 0, j = -1;
+    while (i < len - 1) {
+        if (j == -1 // i is already in the lefemost, we can only shift i to the right
+                || str[i] == str[j]) { // str[0~k-1] == str[i-k~i-1] => str[0~k] == str[i-k~i], next[i+1] == k + 1 == next[i] + 1
+            next[++i] = ++j;
+        } else {
+            j = next[j]; // backtracking
+        }
+    }
+}
+
+int kmp(char text[], char pattern[]) {
+    int i = 0, j = 0;
+    while (i < strlen(text) && j < strlen(pattern)) {
+        if (j == -1 || text[i] == pattern[j]) {
+            ++i; ++j;
+        } else {
+            j = next[j];
+        }
+    }
+    if (j == strlen(pattern)) {
+        return i - j;
+    } else {
+        return -1;
+    }
+}
+
---
+++ zoj/zoj2177.cc
@@ -0,0 +1,34 @@
+/**
+ * ZOJ 2177, Period
+ * KMP
+ */
+#include<cstdio>
+
+char buf[1000005];
+int fail[1000005];
+
+int main(void) {
+    int N, CASES = 1;
+    while (scanf("%d", &N), N) {
+        printf("Test case #%d\n", CASES++);
+        scanf("%s", buf);
+        fail[0] = -1;
+        for (int i = 1, j = -1; i < N; i++) {
+            while (j >= 0 && buf[i] != buf[j + 1]) {
+                // backtracking pointer j
+                j = fail[j];
+            }
+            if (buf[i] == buf[j + 1]) {
+                ++j;
+            }
+            fail[i] = j;
+            if ((j + 1 >= (i + 1) / 2)
+                    && ((i + 1) % (i - j) == 0)) {
+                printf("%d %d\n", i + 1, (i + 1) / (i - j));
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
