7bff911c6ffb83eeceb86f6e7aa3079f9d4c599a
xiaobengdou <jacklinshi1004@gmail.com>
2014-09-26 11:40:36 -0700

    hdu 1089

---
+++ hdu/hdu1089.cc
@@ -0,0 +1,15 @@
+/**
+ * HDU 1089, A+B for Input-Output Practice (II)
+ */
+#include<cstdio>
+
+int main() {
+    int t, a, b;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d%d", &a, &b);
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
+
