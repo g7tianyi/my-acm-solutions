3a993bba9ce19cfd6f1f986f57d4b5808cf50dc7
xiaobengdou <jacklinshi1004@gmail.com>
2014-09-25 17:11:49 -0700

    ural 1001

--- ural/ural1001.cc
+++ ural/ural1001.cc
@@ -1,9 +1,20 @@
+/**
+ * URAL 1001, Reverse Root
+ */
+#include <cmath>
 #include <cstdio>
 
+// 256 * 1024 / sizeof(long)
+double input[1000000];
+
 int main() {
-    int a, b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
+    int i = 0, j;
+    while (scanf("%lf", &input[i]) != EOF) {
+        i++;
+    }
+    for (j = i - 1; j >= 0; j--) {
+        printf("%.4lf\n", sqrt(input[j]));
+    }
     return 0;
 }
 
