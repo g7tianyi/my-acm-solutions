01879d6653f1ba9659f4177196163e570f1cd000
xiaobengdou <jacklinshi1004@gmail.com>
2014-11-05 21:13:06 -0800

    樹問題總結

--- repo/tree.hpp
+++ repo/tree.hpp
@@ -1,11 +1,3 @@
-/**
- * tree operations summary
- *
- * https://github.com/g7tianyi/my-acm-solutions/blob/master/algo/tree-essential-1.md
- *
- * @g7tianyi
- */
-
 #include <functional>
 #include <iostream>
 #include <iterator>
@@ -22,7 +14,6 @@
 #  define LOG_DEBUG(fmt, ...)  do {} while (0)
 #endif
 
-
 namespace tree {
 
 static int const MAX_RAND_NUM = 97;
@@ -34,15 +25,15 @@ inline default_value_type random_tree_value() {
 }
 
 template<typename T>
-struct __tree_node {
+struct tree_node {
     T value;
-    __tree_node <T>* left;
-    __tree_node <T>* right;
-    __tree_node() :
-        value(T()), left(NULL), right(NULL) {
+    tree_node <T>* left;
+    tree_node <T>* right;
+    tree_node() :
+            value(T()), left(NULL), right(NULL) {
     }
-    __tree_node(const T& _value) :
-        value(_value), left(NULL), right(NULL) {
+    tree_node(const T& _value) :
+            value(_value), left(NULL), right(NULL) {
     }
 };
 
@@ -51,8 +42,7 @@ enum insert_type {
 };
 
 enum binary_tree_type {
-    BINARY_RANDOM_TREE,
-    BINARY_SEARCH_TREE
+    BINARY_RANDOM_TREE, BINARY_SEARCH_TREE
 };
 
 template<typename Argument>
@@ -98,7 +88,7 @@ struct tree_node_printer: tree_node_accessor<Argument> {
 };
 
 template<typename Type, typename Accessor = tree_node_printer<Type> >
-void pre_order(__tree_node <Type>* root, const Accessor& accessor =
+void pre_order(tree_node <Type>* root, const Accessor& accessor =
         tree_node_printer<Type>()) {
     if (root != NULL) {
         accessor(root->value);
@@ -108,7 +98,7 @@ void pre_order(__tree_node <Type>* root, const Accessor& accessor =
 }
 
 template<typename Type, typename Accessor = tree_node_printer<Type> >
-void in_order(__tree_node <Type>* root, const Accessor& accessor =
+void in_order(tree_node <Type>* root, const Accessor& accessor =
         tree_node_printer<Type>()) {
     if (root) {
         in_order(root->left);
@@ -118,7 +108,7 @@ void in_order(__tree_node <Type>* root, const Accessor& accessor =
 }
 
 template<typename Type, typename Accessor = tree_node_printer<Type> >
-void post_order(__tree_node <Type>* root,
+void post_order(tree_node <Type>* root,
         const Accessor& accessor = tree_node_printer<Type>()) {
     if (root) {
         post_order(root->left);
@@ -128,12 +118,12 @@ void post_order(__tree_node <Type>* root,
 }
 
 template<typename Type, typename Accessor = tree_node_printer<Type> >
-void level_order(__tree_node <Type>* root,
+void level_order(tree_node <Type>* root,
         const Accessor& accessor = tree_node_printer<Type>()) {
-    std::deque<__tree_node <Type>*> queue;
+    std::deque<tree_node <Type>*> queue;
     queue.push_back(root);
     queue.push_back(NULL);
-    __tree_node <Type>* curr;
+    tree_node <Type>* curr;
     while (!queue.empty()) {
         curr = queue.front();
         queue.pop_front();
@@ -160,12 +150,12 @@ void level_order(__tree_node <Type>* root,
 //
 
 template<typename T, typename Optioner>
-bool __insert_tree(__tree_node <T>* root, __tree_node <T>* node,
+bool __insert_tree(tree_node <T>* root, tree_node <T>* node,
         const Optioner& optioner) {
     if (root == NULL || node == NULL) {
-        return;
+        return false;
     }
-    __tree_node <T>* curr = root;
+    tree_node <T>* curr = root;
     while (true) {
         if (optioner(curr->value, node->value) == SKIP) {
             return false;
@@ -191,16 +181,16 @@ bool __insert_tree(__tree_node <T>* root, __tree_node <T>* node,
 
 template<typename InputIterator, typename Optioner = random_optioner<
         typename InputIterator::value_type> >
-__tree_node <typename InputIterator::value_type>* create_tree(InputIterator first,
+tree_node <typename InputIterator::value_type>* create_tree(InputIterator first,
         InputIterator last,
         const Optioner& optioner =
                 random_optioner<typename InputIterator::value_type>()) {
     typedef typename InputIterator::value_type value_type;
-    __tree_node <value_type>* root = new __tree_node <value_type>(*first);
+    tree_node <value_type>* root = new tree_node <value_type>(*first);
     while (++first != last) {
-        __tree_node <value_type>* node = new __tree_node <value_type>(*first);
+        tree_node <value_type>* node = new tree_node <value_type>(*first);
         if (!__insert_tree(root, node, optioner)) {
-           delete node;
+            delete node;
         }
     }
     return root;
@@ -208,8 +198,8 @@ __tree_node <typename InputIterator::value_type>* create_tree(InputIterator firs
 
 // find element in binary search tree, iterative style
 template<typename T>
-__tree_node <T>* find_elem_in_bst(__tree_node <T>* tree, const T& value) {
-    __tree_node <T>* curr = tree;
+tree_node <T>* find_elem_in_bst(tree_node <T>* tree, const T& value) {
+    tree_node <T>* curr = tree;
     while (curr) {
         if (value == curr->value) {
             return curr;
@@ -225,7 +215,7 @@ __tree_node <T>* find_elem_in_bst(__tree_node <T>* tree, const T& value) {
 
 // find element in binary search tree, recursive style
 template<typename T>
-__tree_node <T>* find_elem_in_bst_recursion(__tree_node <T>* tree, const T& value) {
+tree_node <T>* find_elem_in_bst_recursion(tree_node <T>* tree, const T& value) {
     if (tree == NULL || value == tree->value) {
         return tree;
     }
@@ -239,14 +229,14 @@ __tree_node <T>* find_elem_in_bst_recursion(__tree_node <T>* tree, const T& valu
 
 // find element in binary random tree, iterative style
 template<typename T>
-__tree_node <T>* find_elem_in_brt(__tree_node <T>* tree, const T& value) {
+tree_node <T>* find_elem_in_brt(tree_node <T>* tree, const T& value) {
     if (tree == NULL) {
         return NULL;
     }
     // it is just a tree traversing problem in iterative style
     // below is a pre-order traversing
-    std::stack<__tree_node <T>*> stack;
-    __tree_node <T>* curr = tree;
+    std::stack<tree_node <T>*> stack;
+    tree_node <T>* curr = tree;
     while (curr || !stack.empty()) {
         while (curr) {
             if (value == curr->value) {
@@ -266,11 +256,11 @@ __tree_node <T>* find_elem_in_brt(__tree_node <T>* tree, const T& value) {
 
 // find element in binary search tree, iterative style
 template<typename T>
-__tree_node <T>* find_elem_in_brt_recursion(__tree_node <T>* tree, const T& value) {
+tree_node <T>* find_elem_in_brt_recursion(tree_node <T>* tree, const T& value) {
     if (tree == NULL || value == tree->value) {
         return tree;
     }
-    __tree_node <T>* result = find_elem_in_brt_recursion(tree->left, value);
+    tree_node <T>* result = find_elem_in_brt_recursion(tree->left, value);
     if (result) {
         return result;
     }
@@ -278,7 +268,7 @@ __tree_node <T>* find_elem_in_brt_recursion(__tree_node <T>* tree, const T& valu
 }
 
 template<typename T>
-unsigned count_leaf_node(__tree_node <T>* tree) {
+unsigned count_leaf_node(tree_node <T>* tree) {
     if (tree) {
         if (tree->left == NULL && tree->right == NULL) {
             return 1;
@@ -290,7 +280,7 @@ unsigned count_leaf_node(__tree_node <T>* tree) {
 }
 
 template<typename T>
-unsigned count_all_node(__tree_node <T>* tree) {
+unsigned count_all_node(tree_node <T>* tree) {
     if (tree) {
         return count_all_node(tree->left) + count_all_node(tree->right) + 1;
     }
@@ -298,16 +288,16 @@ unsigned count_all_node(__tree_node <T>* tree) {
 }
 
 template<typename T>
-unsigned min_depth(__tree_node <T>* tree) {
+unsigned min_depth(tree_node <T>* tree) {
     if (tree == NULL) {
         return 0;
     }
 
-    std::deque<__tree_node <T>*> queue;
+    std::deque<tree_node <T>*> queue;
     queue.push_back(tree), queue.push_back(NULL);
     unsigned depth = 1;
     while (!queue.empty()) {
-        __tree_node <T>* curr = queue.front();
+        tree_node <T>* curr = queue.front();
         queue.pop_front();
 
         if (curr) {
@@ -334,7 +324,7 @@ unsigned min_depth(__tree_node <T>* tree) {
 // 1) try BFS
 // 2) simulate recursion with stack
 template<typename T>
-unsigned max_depth(__tree_node <T>* tree) {
+unsigned max_depth(tree_node <T>* tree) {
     if (tree == NULL) {
         return 0;
     }
@@ -346,10 +336,10 @@ unsigned max_depth(__tree_node <T>* tree) {
 // iterative style of max_depth
 // simulate in-order traverse
 template<typename T>
-unsigned max_depth_iteration(__tree_node <T>* tree) {
-    std::stack<__tree_node<T>*> node_stack;
+unsigned max_depth_iteration(tree_node <T>* tree) {
+    std::stack<tree_node <T>*> node_stack;
     std::stack<unsigned> depth_stack;
-    __tree_node <T>* curr = tree;
+    tree_node <T>* curr = tree;
     int depth = 1, result = 0;
     while (curr || !node_stack.empty()) {
         while (curr) {
@@ -373,7 +363,7 @@ unsigned max_depth_iteration(__tree_node <T>* tree) {
 }
 
 template<typename T>
-bool validate_bst(__tree_node <T>* tree, const T& min_elem, const T& max_elem) {
+bool validate_bst(tree_node <T>* tree, const T& min_elem, const T& max_elem) {
     if (tree == NULL) {
         return true;
     }
@@ -388,10 +378,10 @@ bool validate_bst(__tree_node <T>* tree, const T& min_elem, const T& max_elem) {
 
 // in order traverse, the in order sequence should be ordered
 template<typename T>
-bool validate_bst(__tree_node <T>* tree) {
-    std::deque<__tree_node <T>*> queue;
-    std::stack<__tree_node <T>*> stack;
-    __tree_node <T>* curr = tree;
+bool validate_bst(tree_node <T>* tree) {
+    std::deque<tree_node <T>*> queue;
+    std::stack<tree_node <T>*> stack;
+    tree_node <T>* curr = tree;
     while (curr || !stack.empty()) {
         while (curr) {
             stack.push(curr);
@@ -406,9 +396,9 @@ bool validate_bst(__tree_node <T>* tree) {
     }
 
     if (!queue.empty()) {
-        __tree_node <T>* prev = queue.front();
+        tree_node <T>* prev = queue.front();
         queue.pop_front();
-        while(!queue.empty()) {
+        while (!queue.empty()) {
             curr = queue.front();
             if (curr->value < prev->value) {
                 return false;
@@ -430,13 +420,12 @@ bool validate_bst(__tree_node <T>* tree) {
 // since the deletion operation depends on it
 //
 
-
 // find the predecessor of the specified value
 // the value must be in the tree, otherwise we return NULL
 template<typename T>
-__tree_node <T>* predecessor_in_bst(__tree_node <T>* tree, const T& value) {
-    __tree_node <T>* curr = tree; // curr and parent pointer
-    std::deque<__tree_node <T>*> ancestors;
+tree_node <T>* predecessor_in_bst(tree_node <T>* tree, const T& value) {
+    tree_node <T>* curr = tree; // curr and parent pointer
+    std::deque<tree_node <T>*> ancestors;
     ancestors.push_back(NULL);
     while (curr) {
         if (value == curr->value) {
@@ -466,7 +455,7 @@ __tree_node <T>* predecessor_in_bst(__tree_node <T>* tree, const T& value) {
         return curr;
     } else {
         // has no left child
-        __tree_node <T>* parent = ancestors.back();
+        tree_node <T>* parent = ancestors.back();
         ancestors.pop_back();
         while (parent && parent->right != curr) {
             curr = parent;
@@ -480,9 +469,9 @@ __tree_node <T>* predecessor_in_bst(__tree_node <T>* tree, const T& value) {
 // find the successor of the specified value
 // the value must be in the tree, otherwise we return NULL
 template<typename T>
-__tree_node <T>* successor_in_bst(__tree_node <T>* tree, const T& value) {
-    __tree_node <T>* curr = tree; // curr and parent pointer
-    std::deque<__tree_node <T>*> ancestors;
+tree_node <T>* successor_in_bst(tree_node <T>* tree, const T& value) {
+    tree_node <T>* curr = tree; // curr and parent pointer
+    std::deque<tree_node <T>*> ancestors;
     ancestors.push_back(NULL);
     while (curr) {
         if (value == curr->value) {
@@ -510,7 +499,7 @@ __tree_node <T>* successor_in_bst(__tree_node <T>* tree, const T& value) {
         }
         return curr;
     } else {
-        __tree_node <T>* parent = ancestors.back();
+        tree_node <T>* parent = ancestors.back();
         ancestors.pop_back();
         while (parent && parent->left != curr) {
             curr = parent;
@@ -522,7 +511,7 @@ __tree_node <T>* successor_in_bst(__tree_node <T>* tree, const T& value) {
 }
 
 template<typename T>
-__tree_node <T>* max_elem(__tree_node <T>* tree) {
+tree_node <T>* max_elem(tree_node <T>* tree) {
     while (tree && tree->right) {
         tree = tree->right;
     }
@@ -530,7 +519,7 @@ __tree_node <T>* max_elem(__tree_node <T>* tree) {
 }
 
 template<typename T>
-__tree_node <T>* min_elem(__tree_node <T>* tree) {
+tree_node <T>* min_elem(tree_node <T>* tree) {
     while (tree && tree->left) {
         tree = tree->left;
     }
@@ -538,7 +527,108 @@ __tree_node <T>* min_elem(__tree_node <T>* tree) {
 }
 
 template<typename T>
-void release_tree(__tree_node <T>* tree) {
+tree_node <T>* create_mirror(tree_node <T>* tree) {
+    if (tree == NULL) {
+        return NULL;
+    }
+    tree_node<T>* new_tree = new tree_node<T>(tree->value);
+    new_tree->left = create_mirror(tree->right);
+    new_tree->right = create_mirror(tree->left);
+    return new_tree;
+}
+
+// create mirror. iterative style.
+template<typename T>
+tree_node <T>* create_mirror_iteratively(tree_node <T>* tree) {
+    if (tree == NULL) {
+        return NULL;
+    }
+
+    tree_node <T>* newtree = new tree_node<T>(tree->value);
+
+    std::deque<tree_node<T>*> oriqueue, newqueue;
+    oriqueue.push_back(tree);
+    newqueue.push_back(newtree);
+    tree_node<T> *oricurr, *newcurr;
+    while (!oriqueue.empty()) {
+        oricurr = oriqueue.front();
+        oriqueue.pop_front();
+        newcurr = newqueue.front();
+        newqueue.pop_front();
+
+        if (oricurr->left) {
+            newcurr->right = new tree_node<T>(oricurr->left->value);
+            oriqueue.push_back(oricurr->left);
+            newqueue.push_back(newcurr->right);
+        }
+        if (oricurr->right) {
+            newcurr->left = new tree_node<T>(oricurr->right->value);
+            oriqueue.push_back(oricurr->right);
+            newqueue.push_back(newcurr->left);
+        }
+    }
+
+    return newtree;
+}
+
+template<typename T>
+bool is_mirror(tree_node<T>* tree1, tree_node<T>* tree2) {
+    if (tree1 == NULL && tree2 == NULL) {
+        return true;
+    }
+    if (tree1 == NULL && tree2 != NULL) {
+        return false;
+    }
+    if (tree1 != NULL && tree2 == NULL) {
+        return false;
+    }
+    return tree1->value == tree2->value
+            && is_mirror(tree1->left, tree2->right)
+            && is_mirror(tree1->right, tree2->left);
+}
+
+template<typename T>
+bool is_mirror_iterative(tree_node<T>* tree1, tree_node<T>* tree2) {
+    std::deque<tree_node<T>*> queue1, queue2;
+    queue1.push_back(tree1);
+    queue2.push_back(tree2);
+    tree_node<T> *curr1, *curr2;
+
+    while (!queue1.empty()) {
+        curr1 = queue1.front();
+        queue1.pop_front();
+        curr2 = queue2.front();
+        queue2.pop_front();
+
+        if (curr1 == NULL && curr2 == NULL) {
+            continue;
+        }
+
+        if (curr1 == NULL && curr2 != NULL) {
+            return false;
+        }
+        if (curr1 != NULL && curr2 == NULL) {
+            return false;
+        }
+        if (curr1->value != curr2->value) {
+            return false;
+        }
+
+        queue1.push_back(curr1->left);
+        queue2.push_back(curr2->right);
+
+        queue1.push_back(curr1->right);
+        queue2.push_back(curr2->left);
+    }
+
+    return true;
+}
+
+
+
+// release a tree
+template<typename T>
+void release_tree(tree_node <T>* tree) {
     if (tree == NULL) {
         return;
     }
